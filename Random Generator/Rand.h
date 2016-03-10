89920af3ebe0dde6f8f6e8282d6266d5e10097b3
Patryk Drozdz <patrol687@wp.pl>
2016-03-10 15:44:26 +0100

    nie wszystko działa

--- Random Generator/Rand.h
+++ Random Generator/Rand.h
@@ -43,15 +43,17 @@ namespace RandomGenerator {
 
 
 	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::TextBox^  x;
+	private: System::Windows::Forms::TextBox^  X;
+
 
 	private: System::Windows::Forms::RadioButton^  levy;
 	private: System::Windows::Forms::RadioButton^  fibonaccie;
 
 
 	private: System::Windows::Forms::RadioButton^  cauchy;
-	private: System::Windows::Forms::GroupBox^  ploted;
-	private: System::Windows::Forms::TextBox^  n;
+
+	private: System::Windows::Forms::TextBox^  N;
+
 
 	private: System::Windows::Forms::Label^  label2;
 
@@ -70,15 +72,14 @@ namespace RandomGenerator {
 		void InitializeComponent(void)
 		{
 			this->loty = (gcnew System::Windows::Forms::GroupBox());
-			this->gen = (gcnew System::Windows::Forms::Button());
-			this->gauss = (gcnew System::Windows::Forms::RadioButton());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->x = (gcnew System::Windows::Forms::TextBox());
-			this->cauchy = (gcnew System::Windows::Forms::RadioButton());
-			this->fibonaccie = (gcnew System::Windows::Forms::RadioButton());
 			this->levy = (gcnew System::Windows::Forms::RadioButton());
-			this->ploted = (gcnew System::Windows::Forms::GroupBox());
-			this->n = (gcnew System::Windows::Forms::TextBox());
+			this->fibonaccie = (gcnew System::Windows::Forms::RadioButton());
+			this->cauchy = (gcnew System::Windows::Forms::RadioButton());
+			this->gauss = (gcnew System::Windows::Forms::RadioButton());
+			this->gen = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->X = (gcnew System::Windows::Forms::TextBox());
+			this->N = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->loty->SuspendLayout();
 			this->SuspendLayout();
@@ -89,71 +90,12 @@ namespace RandomGenerator {
 			this->loty->Controls->Add(this->fibonaccie);
 			this->loty->Controls->Add(this->cauchy);
 			this->loty->Controls->Add(this->gauss);
-			this->loty->Location = System::Drawing::Point(33, 13);
+			this->loty->Location = System::Drawing::Point(12, 462);
 			this->loty->Name = L"loty";
 			this->loty->Size = System::Drawing::Size(233, 100);
 			this->loty->TabIndex = 0;
 			this->loty->TabStop = false;
 			this->loty->Text = L"Loty";
-			// 
-			// gen
-			// 
-			this->gen->Location = System::Drawing::Point(731, 13);
-			this->gen->Name = L"gen";
-			this->gen->Size = System::Drawing::Size(75, 23);
-			this->gen->TabIndex = 1;
-			this->gen->Text = L"Gen";
-			this->gen->UseVisualStyleBackColor = true;
-			this->gen->Click += gcnew System::EventHandler(this, &RandomGenerator::gen_Click);
-			// 
-			// gauss
-			// 
-			this->gauss->AutoSize = true;
-			this->gauss->Location = System::Drawing::Point(28, 20);
-			this->gauss->Name = L"gauss";
-			this->gauss->Size = System::Drawing::Size(55, 17);
-			this->gauss->TabIndex = 0;
-			this->gauss->TabStop = true;
-			this->gauss->Text = L"Gauss";
-			this->gauss->UseVisualStyleBackColor = true;
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(301, 23);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(14, 13);
-			this->label1->TabIndex = 2;
-			this->label1->Text = L"X";
-			// 
-			// x
-			// 
-			this->x->Location = System::Drawing::Point(321, 20);
-			this->x->Name = L"x";
-			this->x->Size = System::Drawing::Size(100, 20);
-			this->x->TabIndex = 3;
-			// 
-			// cauchy
-			// 
-			this->cauchy->AutoSize = true;
-			this->cauchy->Location = System::Drawing::Point(135, 20);
-			this->cauchy->Name = L"cauchy";
-			this->cauchy->Size = System::Drawing::Size(61, 17);
-			this->cauchy->TabIndex = 1;
-			this->cauchy->TabStop = true;
-			this->cauchy->Text = L"Cauchy";
-			this->cauchy->UseVisualStyleBackColor = true;
-			// 
-			// fibonaccie
-			// 
-			this->fibonaccie->AutoSize = true;
-			this->fibonaccie->Location = System::Drawing::Point(135, 68);
-			this->fibonaccie->Name = L"fibonaccie";
-			this->fibonaccie->Size = System::Drawing::Size(77, 17);
-			this->fibonaccie->TabIndex = 2;
-			this->fibonaccie->TabStop = true;
-			this->fibonaccie->Text = L"Fibonaccie";
-			this->fibonaccie->UseVisualStyleBackColor = true;
 			// 
 			// levy
 			// 
@@ -166,26 +108,76 @@ namespace RandomGenerator {
 			this->levy->Text = L"Levy";
 			this->levy->UseVisualStyleBackColor = true;
 			// 
-			// ploted
+			// fibonaccie
 			// 
-			this->ploted->Location = System::Drawing::Point(1, 124);
-			this->ploted->Name = L"ploted";
-			this->ploted->Size = System::Drawing::Size(837, 447);
-			this->ploted->TabIndex = 4;
-			this->ploted->TabStop = false;
-			this->ploted->Text = L"Wykres";
+			this->fibonaccie->AutoSize = true;
+			this->fibonaccie->Location = System::Drawing::Point(135, 68);
+			this->fibonaccie->Name = L"fibonaccie";
+			this->fibonaccie->Size = System::Drawing::Size(77, 17);
+			this->fibonaccie->TabIndex = 2;
+			this->fibonaccie->TabStop = true;
+			this->fibonaccie->Text = L"Fibonaccie";
+			this->fibonaccie->UseVisualStyleBackColor = true;
 			// 
-			// n
+			// cauchy
 			// 
-			this->n->Location = System::Drawing::Point(571, 16);
-			this->n->Name = L"n";
-			this->n->Size = System::Drawing::Size(100, 20);
-			this->n->TabIndex = 6;
+			this->cauchy->AutoSize = true;
+			this->cauchy->Location = System::Drawing::Point(135, 20);
+			this->cauchy->Name = L"cauchy";
+			this->cauchy->Size = System::Drawing::Size(61, 17);
+			this->cauchy->TabIndex = 1;
+			this->cauchy->TabStop = true;
+			this->cauchy->Text = L"Cauchy";
+			this->cauchy->UseVisualStyleBackColor = true;
+			// 
+			// gauss
+			// 
+			this->gauss->AutoSize = true;
+			this->gauss->Location = System::Drawing::Point(28, 20);
+			this->gauss->Name = L"gauss";
+			this->gauss->Size = System::Drawing::Size(55, 17);
+			this->gauss->TabIndex = 0;
+			this->gauss->TabStop = true;
+			this->gauss->Text = L"Gauss";
+			this->gauss->UseVisualStyleBackColor = true;
+			// 
+			// gen
+			// 
+			this->gen->Location = System::Drawing::Point(728, 470);
+			this->gen->Name = L"gen";
+			this->gen->Size = System::Drawing::Size(75, 23);
+			this->gen->TabIndex = 1;
+			this->gen->Text = L"Gen";
+			this->gen->UseVisualStyleBackColor = true;
+			this->gen->Click += gcnew System::EventHandler(this, &RandomGenerator::gen_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(280, 462);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(14, 13);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"X";
+			// 
+			// X
+			// 
+			this->X->Location = System::Drawing::Point(300, 462);
+			this->X->Name = L"X";
+			this->X->Size = System::Drawing::Size(100, 20);
+			this->X->TabIndex = 3;
+			// 
+			// N
+			// 
+			this->N->Location = System::Drawing::Point(524, 458);
+			this->N->Name = L"N";
+			this->N->Size = System::Drawing::Size(100, 20);
+			this->N->TabIndex = 6;
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(550, 18);
+			this->label2->Location = System::Drawing::Point(489, 461);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(15, 13);
 			this->label2->TabIndex = 5;
@@ -196,13 +188,15 @@ namespace RandomGenerator {
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(838, 574);
-			this->Controls->Add(this->n);
-			this->Controls->Add(this->label2);
-			this->Controls->Add(this->ploted);
-			this->Controls->Add(this->x);
-			this->Controls->Add(this->label1);
-			this->Controls->Add(this->gen);
+			this->Controls->Add(this->N);
 			this->Controls->Add(this->loty);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->X);
+			this->Controls->Add(this->gen);
+			this->Controls->Add(this->label1);
+			this->MaximumSize = System::Drawing::Size(854, 612);
+			this->MinimizeBox = false;
+			this->MinimumSize = System::Drawing::Size(854, 612);
 			this->Name = L"RandomGenerator";
 			this->Text = L"RandomGenerator";
 			this->loty->ResumeLayout(false);
@@ -213,7 +207,36 @@ namespace RandomGenerator {
 		}
 #pragma endregion
 	private: System::Void gen_Click(System::Object^  sender, System::EventArgs^  e) {
+		
+	
+
+		
+
+		if (cauchy->Checked==true) {
+
+
+
+		    Int16 x = Int16::Parse(X->Text);
+			Int16 n = Int16::Parse(N->Text);
+
+			Double pi = 3.14;
+			Double f = 0;
+
+			for (Int16 i = x; i < n; i++) {
+
+				Graphics^ graph = this->CreateGraphics();
+				Pen^ pen = gcnew Pen(System::Drawing::Color::Red);
+				
+				f = (1.0 / pi)*(1.0 / (1.0 + ((Double)i*(Double)i)));
+
+				graph->DrawRectangle(pen, i%(this->Width), (Int16)f%(this->Height), 
+					(i + 10) % (this->Width), (Int16)(f + 10.0) % (this->Height));
+
+			}
+
+		}
 
 	}
+
 };
 }
